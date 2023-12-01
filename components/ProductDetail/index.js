311387489f6762a431b835898d27a0e40440dc41
NguyenLongMoon <longsky0912624119@gmail.com>
2023-12-01 23:42:56 +0700

    cap nhat danh sach san pham lan 2

--- components/ProductDetail/index.js
+++ components/ProductDetail/index.js
@@ -17,9 +17,47 @@ export default function ProductDetail({navigation, route}) {
             </View>
             
 
+            <View style={{flex: 1}}>
+                <Text style={{fontWeight: "500", fontSize: 20}}>{product.name}</Text>
+                <View style={{flexDirection: "row", justifyContent: "space-between", marginVertical: 5, alignItems: "center"}}>
+                    <Text style={{fontSize: 15}}>{product.desc}</Text>
+                    <Text style={{fontSize: 20, fontWeight: "600"}}>${(product.price).toFixed(2)}</Text>
+                </View>
+                <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-between", marginTop: 30}}>
+                    <View>
+                        <View style={{flexDirection: "row", alignItems: "center"}}>
+                            <Image 
+                                source={require("../../assets/timer.png")}
+                                resizeMode='contain'
+                                style={{width: 20, height: 20, marginRight: 10}}
+                            />
+                            <Text style={{fontSize: 15, color: "#666"}}>Delivery in</Text>
+                        </View>
+                        <Text style={{fontSize: 20, fontWeight: "600"}}>30 Min</Text>
+                    </View>
+                    
 
 
-            
+
+
+
+
+                    
+                  
+                    
+                </View>
+                <View>
+                        <Text style={{fontSize: 20, fontWeight: "600", marginBottom: 10}}>Restaurants info</Text>
+                        <Text style={{fontSize: 14, color: "#666", marginBottom: 20}}>
+Order a Large Pizza but the size is the equivalent of a medium/small from other places at the same price range.</Text>
+                    </View>
+                <Pressable>
+                        <Text style={{textAlign: "center", color: "#fff", fontSize: 25
+                        , backgroundColor: "#F1B000", paddingVertical: 10, borderRadius: 4}}>Add to cart</Text>
+                </Pressable>
+            </View>
+
+
         </SafeAreaView>
     </View>
   )
